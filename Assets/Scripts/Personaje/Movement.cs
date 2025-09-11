76c962805aca772b9a3de90079849e7840377fa8
Darwilton-300 <darwilton.herrera.perez@gmail.com>
2025-09-11 11:31:37 -0500

    Creacion del metodo Damage en el script de moviento
    Maldito porfa no modifiques los scripts PORFAVOOOOR

--- Assets/Scripts/Personaje/Movement.cs
+++ Assets/Scripts/Personaje/Movement.cs
@@ -26,4 +26,8 @@ public class Movement : MonoBehaviour
     {
         rb.linearVelocity=(rb.linearVelocity.y * Vector2.up + movent * SpeedForce);    
     }
+    public void Damage()
+    {
+
+    }
 }
